0130b20c4b49c49a2f26b9a38e37b87c306a96d0
malarcukdiana <56016905+malarcukdiana@users.noreply.github.com>
2019-12-16 14:12:58 +0200

    Add files via upload

---
+++ 3_1.py
@@ -0,0 +1,14 @@
+'''Описуємо функцію, яка приймає рядок s і
+повертатиме кількість унікальних символів у ньому'''
+def unique(s):
+    d = {}
+    for symbol in s:    
+        if symbol not in d:
+            d[symbol] = 1   
+        else:               
+            d[symbol] += 1
+    return len(d)           
+
+s = input('Введіть рядок: ') 
+unique_symbols = unique(s)   
+print('Унікальних символів у рядку:', unique_symbols) 
